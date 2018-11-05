83f72902dd5e4705e5293b29b097761949e3402a
P-O-L-I-N-A <Polina.vo@gmail.com>
2018-11-04 23:17:52 -0500

    Trying to revert back to Nov 2

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 'use strict'
 
 const config = require('../config.js')
 
 const signUp = data => {
+=======
+/*
+const config = require('../config.js')
+
+const SignUp = data => {
+>>>>>>> parent of 5faf469... Revert "Commit with Joel's code"
   return $.ajax({
     url: config.apiUrl + '/sign-up',
     method: 'POST',
@@ -10,6 +17,33 @@ const signUp = data => {
   })
 }
 
+<<<<<<< HEAD
 module.exports = {
   signUp
 }
+=======
+const SignIn = data => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const SignOut = data => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    header: {Athorization: 'Token token=' + store.user.token},
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut
+}
+
+*/
+>>>>>>> parent of 5faf469... Revert "Commit with Joel's code"
